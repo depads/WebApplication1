93600e536c1bd5eef077e6587ec6563cf6abaadd
medjiedepadua5 <medjiedepadua5@gmail.com>
2024-03-09 15:55:45 +0800

    first comment

--- WebApplication1/Global.asax.cs
+++ WebApplication1/Global.asax.cs
@@ -11,6 +11,14 @@ namespace WebApplication1
     {
         protected void Application_Start()
         {
+            ViewEngines.Engines.Clear();
+            var razorEngine = new RazorViewEngine();
+            razorEngine.ViewLocationFormats = razorEngine.MasterLocationFormats
+                .Concat(new[] {
+                "~Views/Home/{1}/{0}.cshtml",
+                }).ToArray();
+            ViewEngines.Engines.Add(razorEngine);
+
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
